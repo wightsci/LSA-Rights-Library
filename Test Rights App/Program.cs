76fcc1ef5530ee0cbe9baf15a574bcb79bc9b37e
Stuart Squibb <stuart_squibb@hotmail.co.uk>
2019-04-07 15:06:17 +0100

    Removed unnecessary usings

--- Test Rights App/Program.cs
+++ Test Rights App/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using LSAController;
 namespace TestRightsApp
 {
@@ -13,19 +9,19 @@ namespace TestRightsApp
             LocalSecurityAuthorityController obj = new LocalSecurityAuthorityController();
             Console.WriteLine("Users with NT Right 'Interactive Logon'");
             System.Collections.Generic.IList<string> interactivelist = obj.GetAccountsWithRights("SeInteractiveLogonRight");
-           for (int x = 0; x < interactivelist.Count; x++)
-           {
-                Console.WriteLine("{0}", interactivelist[x]);
-           }
-           Console.ReadKey();
+            for (int x = 0; x < interactivelist.Count; x++)
+                {
+                    Console.WriteLine("{0}", interactivelist[x]);
+                }
+            Console.ReadKey();
             Console.WriteLine();
             Console.WriteLine("NT Rights for the Local Administrators Group");
-           System.Collections.Generic.IList<string> administratorlist = obj.GetRights("Administrators");
-           for (int x = 0; x < administratorlist.Count; x++)
-           {
-                Console.WriteLine("{0}", administratorlist[x]);
-           }
-           Console.ReadKey();
+            System.Collections.Generic.IList<string> administratorlist = obj.GetRights("Administrators");
+            for (int x = 0; x < administratorlist.Count; x++)
+                {
+                    Console.WriteLine("{0}", administratorlist[x]);
+                }
+            Console.ReadKey();
         }
     }
 }
